7c87f0008573ca83aa64b275b600f1defb5193b0
pumpkin-raccoon <pumpkin.raccoon@gmail.com>
2021-05-05 19:40:08 +0900

    Add stylesheet to HomeContainer

---
+++ containers/Home/HomeContainer.tsx
@@ -0,0 +1,13 @@
+import styles from './HomeContainer.module.scss'
+
+const HomeContainer = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        Hi, this is Pumpkin-Raccoon
+      </h2>
+    </div>
+  )
+}
+
+export default HomeContainer
