993cf83d334c6820b1fcf153e917f31b2e71a41c
pumpkin-raccoon <pumpkin.raccoon@gmail.com>
2021-05-17 12:10:30 +0900

    Add eslint and prettier

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,4 +1,6 @@
 const TestPage = () => {
+  let a = 1 // eslint 잡히는지 확인 (미사용, 재설정 아니면 const)
+
   return (
     <div>
       <h1>This is h1</h1>
@@ -6,11 +8,9 @@ const TestPage = () => {
       <h5>This is h5</h5>
       <p>This is p tag</p>
       <button>
-        This is button
+        <p>This is button</p>
       </button>
-      <a href='/'>
-        This is a tag
-      </a>
+      <a href="/">This is a tag</a>
     </div>
     // <div style={{
     //   width: '200px',
