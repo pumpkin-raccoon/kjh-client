2633eae18f3d35c19d333048296e87f3630d46db
pumpkin-raccoon <pumpkin.raccoon@gmail.com>
2021-09-09 09:19:46 +0900

    Remove edit page

--- pages/survey/[code]/edit.tsx
+++ pages/survey/[code]/edit.tsx
@@ -1,9 +1,9 @@
 import { getDefaultSurvey, Survey } from 'models/Survey'
 import { getDefaultUser, User } from 'models/User'
 import { NextPageContext } from 'next'
-import SurveyParticipationContainer from 'containers/SurveyParticipationContainer/SurveyParticipationContainer'
-import { getDefaultSurveyItem } from 'models/SurveyItem'
-import { getDefaultQuestion, QuestionType } from 'models/Question'
+// import SurveyParticipationContainer from 'containers/SurveyParticipationContainer/SurveyParticipationContainer'
+// import { getDefaultSurveyItem } from 'models/SurveyItem'
+// import { getDefaultQuestion, QuestionType } from 'models/Question'
 
 const SurveyParticipationPage = (props: {
   user: User | null
@@ -22,32 +22,33 @@ const SurveyParticipationPage = (props: {
   }
 
   return (
-    <SurveyParticipationContainer
-      user={ { ...getDefaultUser(), ...{
-        name: '이재하',
-        profile: {
-          greetings: '매일매일 성장하는 호박너구리',
-          photoUrl: ''
-        }
-      } } }
-      survey={ { ...getDefaultSurvey(), ...{
-        content: '안녕하세요. 저는 호박너구리 이재하입니다. 여러분들의 솔직한 의견을 통해 스스로 더욱 발전하고 싶어요!',
-        items: [
-          { ...getDefaultSurveyItem(), ...{
-            id: '123',
-            title: '업무로서의 재하는?',
-            content: 'asdfasdf',
-            questions: [
-              { ...getDefaultQuestion(), ...{
-                id: '1231',
-                type: QuestionType.GREEN,
-                content: 'asdfasdf'
-              } }
-            ]
-          } }
-        ]
-      } } }
-    />
+    <></>
+    // <SurveyParticipationContainer
+    //   user={ { ...getDefaultUser(), ...{
+    //     name: '이재하',
+    //     profile: {
+    //       greetings: '매일매일 성장하는 호박너구리',
+    //       photoUrl: ''
+    //     }
+    //   } } }
+    //   survey={ { ...getDefaultSurvey(), ...{
+    //     content: '안녕하세요. 저는 호박너구리 이재하입니다. 여러분들의 솔직한 의견을 통해 스스로 더욱 발전하고 싶어요!',
+    //     items: [
+    //       { ...getDefaultSurveyItem(), ...{
+    //         id: '123',
+    //         title: '업무로서의 재하는?',
+    //         content: 'asdfasdf',
+    //         questions: [
+    //           { ...getDefaultQuestion(), ...{
+    //             id: '1231',
+    //             type: QuestionType.GREEN,
+    //             content: 'asdfasdf'
+    //           } }
+    //         ]
+    //       } }
+    //     ]
+    //   } } }
+    // />
   )
 }
 
